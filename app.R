58be3a1683f700f4b38230e5d47ed24dce6bed23
hunterowens <owens.hunter@gmail.com>
2017-01-25 16:03:30 -0800

    structure sketch

--- app.R
+++ app.R
@@ -9,14 +9,31 @@ library(tidyverse)
 # data_2017 <- read.socrata("https://data.lacity.org/A-Well-Run-City/MyLA311-Service-Request-Data-2017/cfmg-7vj2")
 
 # Read from Local
-data_2016 <- read_csv('./data/MyLA311_Service_Request_Data_2016.csv')
-data_2017 <- read_csv('./data/MyLA311_Service_Request_Data_2017.csv')
+# data_2016 <- read_csv('./data/MyLA311_Service_Request_Data_2016.csv')
+# data_2017 <- read_csv('./data/MyLA311_Service_Request_Data_2017.csv')
 
-full_dataset <- merge(data_2016, data_2017, all=TRUE)
+# full_dataset <- merge(data_2016, data_2017, all=TRUE)
 
-header <- dashboardHeader()
-sidebar <- dashboardSidebar()
-body <-dashboardBody()
+header <- dashboardHeader(title = "LA 311 Dashboard")
+
+sidebar <- dashboardSidebar(
+  sidebarMenu(
+    menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
+    menuItem("Widgets", icon = icon("th"), tabName = "widgets")
+  )
+)
+
+body <- dashboardBody(
+  tabItems(
+    tabItem(tabName = "dashboard",
+            h2("Dashboard tab content")
+    ),
+    
+    tabItem(tabName = "widgets",
+            h2("Widgets tab content")
+    )
+  )
+)
 
 ui <- dashboardPage(header, sidebar, body)
 
