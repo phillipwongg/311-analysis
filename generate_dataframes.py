21b57d0f996daf96a1dc13054530fa6d62a57ffb
hunterowens <owens.hunter@gmail.com>
2017-02-06 19:33:45 -0800

    pipeline

---
+++ generate_dataframes.py
@@ -0,0 +1,19 @@
+#!/bin/python 
+
+## A script that generates dataframes for 311. 
+
+import pandas 
+import feather
+import requests
+
+def get_data():
+    ## download the data
+    pass
+
+data_2017 = pd.read_csv('./data/MyLA311_Service_Request_Data_2017.csv')
+data_2016 = pd.read_csv('./data/MyLA311_Service_Request_Data_2016.csv')
+
+merged = pd.concat(data_2017, data_2016)
+
+
+    
