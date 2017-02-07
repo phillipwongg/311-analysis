b553ab3dc3278264d4c09dab4fd7181952c2ec2b
hunterowens <owens.hunter@gmail.com>
2017-02-07 14:23:18 -0800

    commits

--- generate_dataframes.py
+++ generate_dataframes.py
@@ -15,5 +15,7 @@ data_2016 = pd.read_csv('./data/MyLA311_Service_Request_Data_2016.csv')
 
 merged = pd.concat([data_2017, data_2016])
 
+merged.CreatedDate = pd.to_datetime(merged.CreatedDate)
 
-    
+merged.CreatedDate.apply(lambda x: x.date).value_counts()
+
